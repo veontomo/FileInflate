7308a4283fd6dbe6f3427a2113db168c48a3ce75
Mario Rossi <veontomo@gmail.com>
2015-12-22 11:37:23 +0100

    feat: set up

---
+++ src/fileinflate/FileInflate.java
@@ -0,0 +1,17 @@
+package fileinflate;
+
+/**
+ * Inflates given template with given data
+ * 
+ */
+public class FileInflate {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        Inflator inflator = new Inflator();
+        
+    }
+    
+}
